b8fae970b17e8073a741f5aabe6a66ce97fd0a19
Zeusine <shawon2012.sk@gmail.com>
2024-03-25 22:52:46 +0600

    navbar/profile

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -9,6 +9,12 @@ class Home extends Controller
 	function index()
 	{
 		// code...
-		$this->view('home');
+		$user = $this->load_model('User');
+
+		$data = $user->findAll();
+
+		//$data = $user->where('firstname','john');
+
+		$this->view('home',['rows'=>$data]);
 	}
 }
--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Home</title>
-
-	<link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/all.min.css">
-</head>
-<body>
-
+<?php $this->view('includes/header')?>
+<?php $this->view('includes/nav')?>
+	
 	<div class="container-fluid">
 		<h1><i class="fa fa-plus"></i>This is home</h1>
 	</div>
-</body>
-</html>
+
+	<?php 
+
+		echo "<pre>";
+		print_r($rows);
+	?>
+<?php $this->view('includes/footer')?>
